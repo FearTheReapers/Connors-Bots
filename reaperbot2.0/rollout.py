d05ed180913ed80aad675282f1f3bc1438fa7e1d
cewallac <38520644+cewallac@users.noreply.github.com>
2018-06-07 15:25:54 -0700

    Update rollout.py

--- reaperbot2.0/rollout.py
+++ reaperbot2.0/rollout.py
@@ -35,29 +35,44 @@ class RolloutBot(sc2.BotAI):
 
 #for selecting our Units
         
-        if self.units(REAPER).idle.amount > 10:
+        if self.units(REAPER).idle.amount > 15:
             for reaper in self.units(REAPER).idle:
-                #MAking the initial population for the first 10 reapers
+                #Making the initial population for the first 10 reapers
                 if reaper.tag in self.reapergenes:
                     print("already done")
                 else:
                     if len(self.reaperrewards.values()) > 2:
-                        print(self.reaperrewards)
-                        pick = random.randint(1,3)
                         v = list(self.reaperrewards.values())
                         k = list(self.reaperrewards.keys())
-                        for i in range(3):
-                            if(i == pick):
-                                self.reapergenes[reaper.tag] = self.reapergenes[k[v.index(max(v))]]
-                                if random.randint(1,100)/100 <= .50:
-                                    #mutate
-                                    pick = random.randint(0,2)
-                                    if(random.randint(1,2) == 1):
-                                        self.reapergenes[reaper.tag][pick] -= .02
-                                    else:
-                                        self.reapergenes[reaper.tag][pick] += .02
+                        curmax = 0
+                        for i in range(10):
+                            pick = random.randint(0,len(v)-1)
+                            if(v[pick] >= curmax):
+                                curmax = v[pick]
+                                curpick = k[pick]
+                        
+                        self.reapergenes[reaper.tag] = [0,0,0]       
+                        self.reapergenes[reaper.tag][0] = self.reapergenes[curpick][0]
+                        self.reapergenes[reaper.tag][1] = self.reapergenes[curpick][1]
+                        self.reapergenes[reaper.tag][2] = self.reapergenes[curpick][2]
+                       
+                        
+                        if random.randint(1,100)/100 <= .50:
+                            print("mutate")
+                            print([reaper.tag, self.reapergenes[reaper.tag], "Son of:", curpick, self.reapergenes[curpick] ,self.reaperrewards[curpick]])
+                            #mutate
+                            pick = random.randint(0,2)
+                            if(random.randint(1,2) == 1):
+                                print("minus .1")
+                                self.reapergenes[reaper.tag][pick] -= .3
+                            else:
+                                print("plus .1")
+                                self.reapergenes[reaper.tag][pick] += .3
+                        
+                        print([reaper.tag, self.reapergenes[reaper.tag], "Son of:", curpick, self.reapergenes[curpick] ,self.reaperrewards[curpick]])
                     else: #since there are no rewarded reapers
                         seed = [0,0,0]
+                        
                         seed[0] = (random.randint(1,100)/100)
                         seed[1] = (random.randint(1,100)/100)
                         seed[2] = (random.randint(1,100)/100)
@@ -66,7 +81,7 @@ class RolloutBot(sc2.BotAI):
 
                                 
                         #del v[v.index(max(v))]
-        
+            print(self.reapergenes)
         
                     
             cg = ControlGroup(self.units(REAPER).idle)
@@ -87,7 +102,7 @@ class RolloutBot(sc2.BotAI):
 #For building reapers     
         if self.units(BARRACKS).amount < 3 or (self.minerals > 400 and self.units(BARRACKS).amount < 5):
             if self.can_afford(BARRACKS):
-                err = await self.build(BARRACKS, near=cc.position.towards(self.game_info.map_center, 5))       
+                err = await self.build(BARRACKS, near=cc.position.towards(self.game_info.map_center, 5))      
            
         elif self.units(BARRACKS).ready.exists and self.units(REFINERY).ready.exists:
             barracks = self.units(BARRACKS).ready
@@ -132,11 +147,10 @@ class RolloutBot(sc2.BotAI):
             total_x = []
             total_y = []
             total_z = []
-            if alive_units.amount > 5:
+            if alive_units.amount > 10:
+                #Calculating which units are better to attack
                 for reaper in ac.select_units(self.units):
                 
-                    if reaper.tag in self.reaperrewards:
-                        print(self.reaperrewards[reaper.tag])
                         
                     ting = 10
                     targets = self.known_enemy_units.prefer_close_to(reaper)
@@ -166,30 +180,47 @@ class RolloutBot(sc2.BotAI):
                 else:
                     enemystart = 1
                     target = self.enemy_start_locations[0]
-                    
+                
+                #For loop deciding what a reaper should do
                 for reaper in ac.select_units(self.units):
+                    
+                    
                     if reaper.tag in self.reaper_health and reaper.tag in self.reapergenes:
+                        #If you've been hit kite away towards a safe place based on your genes
                         if  reaper.health < self.reaper_health[reaper.tag]:
-                            await self.do(reaper.move(reaper.position.towards(target, self.reapergenes[reaper.tag][2]*-5)))
-                        elif reaper.health < reaper.health_max:
+                            await self.do(reaper.move(reaper.position.towards(barracks.random.position, self.reapergenes[reaper.tag][2]*10)))
+                        
+                        #if you are too injured to fight, run away
+                        elif reaper.health < reaper.health_max*.4:
                             await self.do(reaper.move(barracks.random.position))
+                        #if you aren't attacking or running you have to fight
                         elif reaper.is_idle:
                             await self.do(reaper.attack(target))
-                            
-                    if reaper.tag in self.reaper_health:        
-                        self.reaperrewards[reaper.tag] = 60 - (self.reaper_health[reaper.tag] - reaper.health)
                     
+                    #Setting up a reward slot for new reapers
+                    if reaper.tag not in self.reaper_health:
+                        self.reaperrewards[reaper.tag] = 0;
+                    
+                    #rewarding reapers for taking damage and surviving it        
+                    if reaper.tag in self.reaper_health:        
+                        self.reaperrewards[reaper.tag] += 60 - reaper.health
+                    
+                    #rewarding reapers for Killing units
                     if reaper.tag in self.reaperlasttarget:
+                        #reward for a unit killed
                         if not self.reaperlasttarget[reaper.tag].health > 0:
-                            self.reaperrewards[reaper.tag] += 100
+                            self.reaperrewards[reaper.tag] += 2000
                             del self.reaperlasttarget[reaper.tag]
-                            
+                        #reward for a unit maimed
                         elif self.reaperlasttarget[reaper.tag].health < self.reaperlasttargethealth[reaper.tag]:
                             self.reaperrewards[reaper.tag] += .1*(self.reaperlasttargethealth[reaper.tag] - self.reaperlasttarget[reaper.tag].health)
+                    
+                    #resetting sentinel dictionaries
                     if len(self.enemyreward.values()) > 0:    
                         self.reaperlasttarget[reaper.tag] = selectedtarget
                         self.reaperlasttargethealth[reaper.tag] = selectedtarget.health
                         self.reaper_health[reaper.tag] = reaper.health
+            #if we don't have enough units to feasably continue the fight run back home
             else:
                 for reaper in ac.select_units(self.units):
                     await self.do(reaper.move(cc.position))
@@ -199,5 +230,5 @@ class RolloutBot(sc2.BotAI):
         
 run_game(maps.get("Simple64"), [
 	Bot(Race.Terran, RolloutBot([1,1,1])),
-	Computer(Race.Zerg, Difficulty.Medium)
-], realtime=False, save_replay_as="GeneticBot1.SC2Replay")
+	Computer(Race.Protoss, Difficulty.Medium)
+], realtime=False, save_replay_as="GeneticBotEASY2.SC2Replay")
